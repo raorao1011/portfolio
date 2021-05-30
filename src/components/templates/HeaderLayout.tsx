921f7c3155caeb1be13b0af84c2007dbfa47b5ef
RAO <raorao1011@gmail.com>
2021-05-30 23:56:31 +0900

    ✨ init commit

---
+++ src/components/templates/HeaderLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { memo, ReactNode, VFC } from "react";
+
+import { Header } from "../organism/layout/Header";
+
+
+export const HeaderLayout = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
---
+++ src/router/Router.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Router = () => {
+  return (
+    <div>
+      <p>a</p>
+    </div>
+  )
+}
