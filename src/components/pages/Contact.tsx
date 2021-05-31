abc2db1253c80f40f4a9656d4062c942226d5ce9
RAO <raorao1011@gmail.com>
2021-05-31 22:41:44 +0900

    ✨ react-hook-form導入

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,9 +1,51 @@
-import React, { VFC } from "react";
+import * as React from 'react'
+import { useForm } from 'react-hook-form'
+import { Input, Button } from '@chakra-ui/react'
+import { FormErrorMessage, FormLabel, FormControl } from '@chakra-ui/react'
 
-export const Contact: VFC = () => {
+type FormData = {
+  firstName: string
+  lastName: string
+}
+
+export function Contact() {
+  //
+  const {
+    register,
+    handleSubmit,
+    formState,
+    formState: { errors },
+  } = useForm<FormData>({
+    mode: 'all',
+  })
+
+  // const onSubmit = handleSubmit((data) => console.log(data))
+
+  function onSubmit(values: any) {
+    return new Promise(() => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2))
+      }, 1000)
+    })
+  }
+
   return (
-    <div>
-      <p>contactページです。</p>
-    </div>
-  );
-};
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl id="firstName" isInvalid={!!errors.firstName} isRequired>
+        <FormLabel>First Name</FormLabel>
+        <Input placeholder="first name" {...register('firstName', { required: true })} />
+        <FormErrorMessage>{errors.firstName && 'First name is required'}</FormErrorMessage>
+      </FormControl>
+
+      <FormControl id="lastName" isInvalid={!!errors.lastName} isRequired>
+        <FormLabel>Last Name</FormLabel>
+        <Input placeholder="last name" {...register('lastName', { required: true })} />
+        <FormErrorMessage>{errors.lastName && 'Last name is required'}</FormErrorMessage>
+      </FormControl>
+
+      <Button mt={4} colorScheme="teal" isLoading={formState.isSubmitting} type="submit">
+        Submit
+      </Button>
+    </form>
+  )
+}
