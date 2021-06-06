7746d4bbc5be72ee6359da274eca5972642deeb0
RAO <raorao1011@gmail.com>
2021-06-07 00:25:00 +0900

    🎨 formの改修

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,12 +1,24 @@
 import * as React from "react";
 import { useForm } from "react-hook-form";
-import { Input, Button, Box, Flex } from "@chakra-ui/react";
+import { Input, Button, Box, Flex, Stack, Heading } from "@chakra-ui/react";
 import { FormErrorMessage, FormLabel, FormControl } from "@chakra-ui/react";
 
+// 入力する値の型定義
 type FormData = {
-  firstName: string;
-  lastName: string;
+  name: string;
+  email: string;
+  contents: string;
 };
+
+/**
+ * 
+  フックから必要なオブジェクトを分割代入
+  register:  バリデーションをチェックするinputをフックに登録するためのものである
+  handleSubmit:
+  formState:
+  formState: { errors }:
+  初期値mode: "all": どの状態でvalidationを発動させるか, allだとblur,change eventで発動するようになる
+ */
 
 export function Contact() {
   //
@@ -16,7 +28,7 @@ export function Contact() {
     formState,
     formState: { errors },
   } = useForm<FormData>({
-    mode: "all",
+    mode: "onSubmit",
   });
 
   // const onSubmit = handleSubmit((data) => console.log(data))
@@ -28,43 +40,71 @@ export function Contact() {
       }, 1000);
     });
   }
+  // JSON.stringifyは入力した値をJSON文字列で返す
+  // JSON.stringify(value[, replacer[, space]])
+  // value: JSON 文字列に変換する値
+  // replacer(任意):もし関数である場合、文字列化の間に出会った値とプロパティを変換します。もし配列である場合は、最終的な文字列のオブジェクトに含まれるプロパティの集合を指定する。
+  // space(任意): 結果の文字列を整形して出力する
+
+  /**
+   * 
+    !!は!がtrue or falseになるのを利用して変数をfalse or trueに変換している
+    isInvaild:無効かどうか
+
+    handleSubmitにフォームの送信ロジックを記述した関数onSubmitを渡すことで
+    送信ボタンが押された際に設定したonSubmitが実行される
+   */
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} style={{ paddingTop: "90px" }}>
-      <Flex justify="center" textAlign="center">
-        <Box w="sm" p={4} borderRadius="md" shadow="md" bg="gray.50">
-          <FormControl id="firstName" isInvalid={!!errors.firstName} isRequired>
-            <FormLabel>First Name</FormLabel>
-            <Input
-              placeholder="first name"
-              {...register("firstName", { required: true })}
-            />
-            <FormErrorMessage>
-              {errors.firstName && "First name is required"}
-            </FormErrorMessage>
-          </FormControl>
-
-          <FormControl id="lastName" isInvalid={!!errors.lastName} isRequired>
-            <FormLabel>Last Name</FormLabel>
-            <Input
-              placeholder="last name"
-              {...register("lastName", { required: true })}
-            />
-            <FormErrorMessage>
-              {errors.lastName && "Last name is required"}
-            </FormErrorMessage>
-          </FormControl>
-
-          <Button
-            mt={4}
-            colorScheme="teal"
-            isLoading={formState.isSubmitting}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Box>
-      </Flex>
-    </form>
+    <>
+      <Heading as="h1" mt={1 / 10} fontSize="3xl">
+        CONTACT
+      </Heading>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ paddingTop: "15%" }}>
+        <Flex justify="center" textAlign="center">
+          <Box w="lg" p={4} borderRadius="md" shadow="md" bg="gray.50">
+            <Stack spacing={4}>
+              <FormControl id="name" isInvalid={!!errors.name} >
+                <FormLabel>お名前(必須)</FormLabel>
+                <Input
+                  placeholder="山田  太郎"
+                  {...register("name", { required: true })}
+                />
+                <FormErrorMessage>{errors.name && "お名前を入力してください"}</FormErrorMessage>
+              </FormControl>
+              <FormControl id="email" isInvalid={!!errors.email} >
+                <FormLabel>メールアドレス(必須)</FormLabel>
+                <Input
+                  placeholder="test@example.com"
+                  {...register("email", { required: true })}
+                />
+                <FormErrorMessage>
+                  {errors.name && "メールアドレスを入力してください"}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl id="contents" isInvalid={!!errors.contents} >
+                <FormLabel>お問い合わせ内容(必須)</FormLabel>
+                <Input
+                  placeholder="例） こんにちは"
+                  {...register("contents", { required: true })}
+                />
+                <FormErrorMessage>
+                  {errors.contents && "お問い合わせ内容を入力してください"}
+                </FormErrorMessage>
+              </FormControl>
+            </Stack>
+            <Button
+              mt={4}
+              colorScheme="teal"
+              loadingText="送信中"
+              isLoading={formState.isSubmitting}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Box>
+        </Flex>
+      </form>
+    </>
   );
 }
